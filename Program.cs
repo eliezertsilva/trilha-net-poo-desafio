68b14d204941a3393773cd807418d72eb5a2cdfa
Leonardo Buta <leonardo.lbuta@gmail.com>
2022-05-22 22:01:58 -0300

    Inicial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Nokia", imei: "11111", memoria: "64");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "54321", modelo: "Iphone", imei: "22222", memoria: "128");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
